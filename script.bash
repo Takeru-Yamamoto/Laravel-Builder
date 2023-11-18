9df93ebec5860b5971e68b6115b1676d296ead6c
takeru <take.yama0519@gmail.com>
2023-11-19 02:05:37 +0900

    Update Script

--- script.bash
+++ script.bash
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
 
-# 変数の受け取り
+# #############################################################################
+# 変数定義
+# #############################################################################
 
-## Laravel をインストールするディレクトリを受け取り
+# Laravel をインストールするディレクトリ
 readonly INSTALL_DIR="$1"
 if [[ -z "${INSTALL_DIR}" ]]; then
     echo "Please enter the directory name to install Laravel."
     exit 1
 fi
 
-## Laravel をインストールするディレクトリの所有権を持つユーザーを受け取り
+# Laravel をインストールするディレクトリの所有権を持つユーザー
 readonly OWNER_USER="$2"
 if [[ -z "${OWNER_USER}" ]]; then
     echo "Please enter the user who owns the directory to install Laravel."
     exit 1
 fi
 
-## Laravel にアクセスする為の URL を受け取り
+# Laravel にアクセスする為の URL
 readonly ACCESS_URL="$3"
 if [[ -z "${ACCESS_URL}" ]]; then
     echo "Please enter the URL to access Laravel."
     exit 1
 fi
 
-## Laravel-Customizedをインストールするかどうかを受け取り
+# Laravel-Customizedをインストールするかどうか
 readonly IS_INSTALL_LATEST_LARAVEL=${4-0}
+
+# #############################################################################
+# 処理部分
+# #############################################################################
 
 # Laravel-Library-Installerの実行
 curl -s https://raw.githubusercontent.com/Takeru-Yamamoto/Laravel-Library-Installer/script.bash | bash
